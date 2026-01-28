af349298ccb9d76d0e8789db53c675a871f4d184
benmedjaldichouaib <119789723+benmedjaldichouaib@users.noreply.github.com>
2026-01-28 14:20:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -88,5 +88,8 @@ def index():
 # Run App
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # إذا Local، يستعمل 10000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
     
